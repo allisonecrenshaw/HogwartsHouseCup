d5f17bb80702556c1e3a26b3a07a775031969fe8
Allison Crenshaw <allisoncrenshaw@Allisons-MacBook-Air.local>
2022-05-28 17:42:10 -0500

    JavaScript code added to toggle flag based on house button clicked.

--- js/main.js
+++ js/main.js
@@ -1,24 +1,27 @@
-// House -button Event Variables
+// Global Vars
 const houseButtons = document.querySelectorAll(".house-button");
 console.log(houseButtons);
 for (const button of houseButtons) {
   button.addEventListener("click", showFlags);
 }
+const HOUSE_FLAG_FILES = {
+  "gryffindor-button": "./Images/GryffindorFlag.png",
+  "hufflepuff-button": "./Images/HufflepuffFlag.png",
+  "slytherin-button": "./Images/SlytherinFlag.png",
+  "ravenclaw-button": "./Images/RavenclawFlag.png",
+};
 
 // House Button Event Listeners
 function showFlags(event) {
+  // console log and setting vars
   console.log(event);
   const eventTargetId = event.target.id;
   console.log(eventTargetId);
-  if (eventTargetId === "gryffindor-button") {
-    alert("Gryffindor Wins!");
-  } else if (eventTargetId === "hufflepuff-button") {
-    alert("Hufflepuff Wins!");
-  } else if (eventTargetId === "slytherin-button") {
-    alert("Slytherin Wins!");
-  } else if (eventTargetId === "ravenclawbutton") {
-    alert("Ravenclaw Wins!");
-  } else {
-    alert("No winner chosen yet.");
+
+  // grab image to change
+  const flagImages = document.querySelectorAll(".flag");
+  // display appropriate flag based on button clicked
+  for (const flagImage of flagImages) {
+    flagImage.src = HOUSE_FLAG_FILES[eventTargetId];
   }
 }
